772f182009d4509acbfda304bd11a6fe69bc8a56
Nathan <nathan.froeh@gmail.com>
2019-07-31 10:01:17 -0600

    Add base components

---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+// img, name, species, description
+
+export class Card extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Card
---
+++ src/CardContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class CardContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+
+export default CardContainer
